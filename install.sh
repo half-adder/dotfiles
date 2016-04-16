41be7bf4d72c1355e6450ca6e3a84a6a6cbf8233
Sean Johnsen <seanjohnsen96@gmail.com>
2016-04-15 21:25:36 -0400

    update install.sh so that it uses the brewfile to install apps... only works if homebrew is installed

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+echo "###################################################"
+echo "installing homebrew..."
 
+echo "###################################################"
+echo "installing apps from Brewfile..."
+brew bundle
+
+# Symlink the dotfiles to the parent directory
+echo "###################################################"
+echo "symlinking dotfiles into parent directory..."
 for dir in  `find * -type d -maxdepth 0 \( -iname ".*" \)`
 do
     stow $dir 
 done
+echo "done!"
