5880a47703371fbd91128af1a6d8bdd2311b70bc
Sean Johnsen <seanjohnsen96@gmail.com>
2016-04-17 02:10:43 -0400

    check for homebrew/installed apps before downloading and installing

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-echo "###################################################"
-echo "installing homebrew..."
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "###################################################"
+    echo "installing homebrew..."
+else
+    echo "###################################################"
+    echo "updating homebrew..."
+    brew update
+fi
 
-echo "###################################################"
-echo "installing apps from Brewfile..."
-brew bundle
+echo "###################################################";
+echo "installing apps from Brewfile...";
+brew bundle check
+if [[ $? != 0 ]] ; then
+    brew bundle;
+fi
 
 # Symlink the dotfiles to the parent directory
 echo "###################################################"
